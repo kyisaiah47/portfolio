576b33a3c6e82283015cbac8ed912944c2b08eaf
Isaiah <isaiah.kim@sscinc.com>
2025-08-03 21:11:42 -0400

    asdf

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
 import {
 	Sidebar,
 	SidebarContent,
@@ -38,7 +42,6 @@ import {
 	BookA,
 	FolderCode,
 } from "lucide-react";
-import Image from "next/image";
 
 export function AppSidebar() {
 	return (
@@ -47,7 +50,10 @@ export function AppSidebar() {
 				<SidebarMenu>
 					<SidebarMenuItem>
 						<SidebarMenuButton asChild>
-							<a>
+							<Link
+								href="/"
+								className="flex items-center gap-2"
+							>
 								<Image
 									className="mr-2"
 									src="https://hvnbpd9agmcawbt2.public.blob.vercel-storage.com/logo-1"
@@ -56,7 +62,7 @@ export function AppSidebar() {
 									height={20}
 								/>
 								<span className="font-bold">Isaiah Kim</span>
-							</a>
+							</Link>
 						</SidebarMenuButton>
 					</SidebarMenuItem>
 				</SidebarMenu>
@@ -68,172 +74,180 @@ export function AppSidebar() {
 						<SidebarMenu>
 							<SidebarMenuItem>
 								<SidebarMenuButton asChild>
-									<a href="/about">
+									<Link href="/about">
 										<CircleUser />
 										<span>About</span>
-									</a>
+									</Link>
 								</SidebarMenuButton>
 							</SidebarMenuItem>
 						</SidebarMenu>
 					</SidebarGroupContent>
 				</SidebarGroup>
+
 				<SidebarSeparator />
+
 				<SidebarGroup>
 					<SidebarGroupLabel>Experience</SidebarGroupLabel>
 					<SidebarGroupContent>
 						<SidebarMenu>
 							<SidebarMenuItem>
 								<SidebarMenuButton asChild>
-									<a
+									<Link
 										href="/ssnc"
 										className="flex items-center gap-2"
 									>
 										<Building2 />
 										<PiggyBank />
 										<span>SS&C Technologies</span>
-									</a>
+									</Link>
 								</SidebarMenuButton>
 							</SidebarMenuItem>
 							<SidebarMenuItem>
 								<SidebarMenuButton asChild>
-									<a
+									<Link
 										href="/capital-technology-group"
 										className="flex items-center gap-2"
 									>
 										<Landmark />
 										<IdCardLanyard />
 										<span>US Immigration (via Capital Technology Group)</span>
-									</a>
+									</Link>
 								</SidebarMenuButton>
 							</SidebarMenuItem>
 							<SidebarMenuItem>
 								<SidebarMenuButton asChild>
-									<a
+									<Link
 										href="/no-name-charli"
 										className="flex items-center gap-2"
 									>
 										<Sprout />
 										<Images />
 										<span>No Name Charli</span>
-									</a>
+									</Link>
 								</SidebarMenuButton>
 							</SidebarMenuItem>
 							<SidebarMenuItem>
 								<SidebarMenuButton asChild>
-									<a
+									<Link
 										href="/visneta"
 										className="flex items-center gap-2"
 									>
 										<Sprout />
 										<Construction />
 										<span>Visneta</span>
-									</a>
+									</Link>
 								</SidebarMenuButton>
 							</SidebarMenuItem>
 						</SidebarMenu>
 					</SidebarGroupContent>
 				</SidebarGroup>
+
 				<SidebarSeparator />
+
 				<SidebarGroup>
 					<SidebarGroupLabel>Personal Projects</SidebarGroupLabel>
 					<SidebarGroupContent>
 						<SidebarMenu>
 							<SidebarMenuItem>
 								<SidebarMenuButton asChild>
-									<a
+									<Link
 										href="/storybook"
 										className="flex items-center gap-2"
 									>
 										<BookA />
 										<FolderCode />
 										<span>Storybook Components</span>
-									</a>
+									</Link>
 								</SidebarMenuButton>
 							</SidebarMenuItem>
 							<SidebarMenuItem>
 								<SidebarMenuButton asChild>
-									<a
+									<Link
 										href="/opensource"
 										className="flex items-center gap-2"
 									>
 										<Github />
 										<FolderCode />
 										<span>Open Source Contributions</span>
-									</a>
+									</Link>
 								</SidebarMenuButton>
 							</SidebarMenuItem>
 						</SidebarMenu>
 					</SidebarGroupContent>
 				</SidebarGroup>
+
 				<SidebarSeparator />
+
 				<SidebarGroup>
 					<SidebarGroupLabel>Hackathons</SidebarGroupLabel>
 					<SidebarGroupContent>
 						<SidebarMenu>
 							<SidebarMenuItem>
 								<SidebarMenuButton asChild>
-									<a
+									<Link
 										href="/sonder"
 										className="flex items-center gap-2"
 									>
 										<BanknoteArrowUp />
 										<Smartphone />
 										<span>RevenueCat Shipaton 2025</span>
-									</a>
+									</Link>
 								</SidebarMenuButton>
 							</SidebarMenuItem>
 							<SidebarMenuItem>
 								<SidebarMenuButton asChild>
-									<a
+									<Link
 										href="/tableau"
 										className="flex items-center gap-2"
 									>
 										<LayoutDashboard />
 										<ChartNoAxesCombined />
 										<span>Tableau Next Virtual Hackathon</span>
-									</a>
+									</Link>
 								</SidebarMenuButton>
 							</SidebarMenuItem>
 							<SidebarMenuItem>
 								<SidebarMenuButton asChild>
-									<a
+									<Link
 										href="/deep-cut"
 										className="flex items-center gap-2"
 									>
 										<Brain />
 										<Ghost />
 										<span>Code with Kiro Hackathon</span>
-									</a>
+									</Link>
 								</SidebarMenuButton>
 							</SidebarMenuItem>
 							<SidebarMenuItem>
 								<SidebarMenuButton asChild>
-									<a
+									<Link
 										href="/qloo-hackathon"
 										className="flex items-center gap-2"
 									>
 										<Brain />
 										<UsersRound />
 										<span>Qloo LLM Hackathon</span>
-									</a>
+									</Link>
 								</SidebarMenuButton>
 							</SidebarMenuItem>
 							<SidebarMenuItem>
 								<SidebarMenuButton asChild>
-									<a
+									<Link
 										href="/hackfs-2022"
 										className="flex items-center gap-2"
 									>
 										<Trophy />
 										<Medal />
 										<span>HackFS 2022</span>
-									</a>
+									</Link>
 								</SidebarMenuButton>
 							</SidebarMenuItem>
 						</SidebarMenu>
 					</SidebarGroupContent>
 				</SidebarGroup>
+
 				<SidebarSeparator />
+
 				<SidebarGroup>
 					<SidebarGroupLabel>Contact</SidebarGroupLabel>
 					<SidebarGroupContent>
